0fe705f4271eff577661a1aa50d77a71a9acb96b
hironobuGOTO <h.goto@denlabo.co.jp>
2022-10-05 15:09:38 +0900

    Add: コメントの追記

--- m5stack-env-monitor.ino
+++ m5stack-env-monitor.ino
@@ -1,28 +1,31 @@
 #include "config.h"
 #include <M5Stack.h>
 #include <WiFi.h>
-#include <Adafruit_SGP30.h>
-Adafruit_SGP30 sgp;
-uint16_t eco2_base = 37335; //eCO2 baseline仮設定値
-uint16_t tvoc_base = 40910; //TVOC baseline仮設定値
 
 #include <Adafruit_NeoPixel.h>
 #define PIN 15
 #define NUMPIXELS 10
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+// 計測機用のライブラリ
 #include "M5_ENV.h"
+#include <Adafruit_SGP30.h>
 #include <Adafruit_BMP280.h>
 #include "Adafruit_Sensor.h"
 
+Adafruit_SGP30 sgp;
 Adafruit_BMP280 bmp280;
 SHT3X sht30;
+
+uint16_t eco2_base = 37335; //eCO2 baseline仮設定値
+uint16_t tvoc_base = 40910; //TVOC baseline仮設定値
 
 // ESP8266Audioライブラリ
 #include "AudioFileSourceSD.h"
 #include "AudioGeneratorMP3.h"
 #include "AudioOutputI2S.h"
 
+// 設定値をJSONに書き出す際に必要なライブラリ
 #include <ArduinoJson.h>
 
 // SHT30で計測した値の構造体
@@ -32,15 +35,11 @@ struct SensorValue {
   float pressure;
 };
 
-
 // SGP30で計測したeCO2濃度の閾値の構造体
 struct ReminderThreshold {
   int attention;
   int caution;
 };
-
-// 不快指数
-int discomfortIndex = 0;
 
 // 24bitカラーの構造体
 struct RGB {
@@ -49,16 +48,19 @@ struct RGB {
   int b;
 };
 
+// 状態がもつ色の構造体の定義
 struct StatusColor {
   String name;
   struct RGB color;
 };
 
+// ECO2計測値の閾値を超えたときの警告色の定義
 struct StatusColor Eco2Color[2] = {
   {"colorAttention", 255, 215, 0},
   {"colorCaution", 255, 69, 0}
 };
 
+// 不快指数が快適じゃないときの警告色の定義
 struct StatusColor DiscomfortColor[6] = {
   {"colorCold", 0, 0, 205},
   {"colorChilly", 135, 206, 235},
@@ -68,11 +70,10 @@ struct StatusColor DiscomfortColor[6] = {
   {"colorBoiling", 255, 127, 80}
 };
 
-
-// 不快指数状況の初期値
+// 不快指数の定義 (これの状態が変化したときに描画を変えるため、グローバルで保持)
 String discomfortStatus = "comfort";
 
-// バックライトの輝度を操作する値 0〜5, 初期値は中央値
+// バックライトの輝度を操作する値 0〜5 (初期値は中央値)
 unsigned char backlightCnt = 2;
 
 // 寝室モードかを確認するフラグ
@@ -90,9 +91,6 @@ bool tmpBedroomModeFlg;
 
 // 最後にCSVに出力した時間を保管しておく変数
 int setCsvWroteTime = 0;
-
-// 時間をtm型で取得する変数
-struct tm currentDateTime;
 
 // Sprite クラスのインスタンス化
 TFT_eSprite sprite = TFT_eSprite(&M5.Lcd);
@@ -117,11 +115,13 @@ void setup() {
   // SDカードに保存された設定情報を読み込み、初期値に反映させる
   File configFile = SD.open("/config.txt", FILE_READ);
   char configArray[configFile.size()];
-  // 設定ファイルの読み込み
+  // 設定ファイルの読み込み (1文字ずつ)
   for (int i = 0; i < configFile.size(); i++) {
     configArray[i] = configFile.read();
   }
   configFile.close();
+
+  // JSONを読んで配列に変換
   DynamicJsonDocument configJson(1024);
   deserializeJson(configJson, configArray);
 
@@ -155,9 +155,8 @@ void setup() {
   sgp.setIAQBaseline(eco2_base, tvoc_base);
   // M5Stack側面LEDの起動
   pixels.begin();
-  M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.printf("SGP30 waking...");
   // SGP30が動作するまで15秒起動中の表示を出す
+  M5.Lcd.printf("\nSGP30 waking");
   for (int i = 0; i < 15; i++) {
     M5.Lcd.printf(".");
     delay(1000);
@@ -172,7 +171,6 @@ void setup() {
   sprite.setTextFont(4);
   sprite.setTextSize(1);
   sprite.createSprite(M5.Lcd.width(), M5.Lcd.height());
-
 }
 
 //温度、相対湿度から絶対湿度を計算する関数
@@ -217,8 +215,6 @@ void measureSensorValues(struct SensorValue& latestSensorValue) {
     Serial.println("Measurement failed");
     while (1);
   }
-  // 不快指数の計算
-  discomfortIndex = ((0.81 * latestSensorValue.temperature) + ((0.01 * latestSensorValue.humidity) * ((0.99 * latestSensorValue.temperature) - 14.3)) + 46.3);
 }
 
 // 画面表示する関数
@@ -228,6 +224,8 @@ void updateScreen(struct SensorValue latestSensorValue) {
     sprite.fillScreen(TFT_BLACK);
     discomfortStatus = "comfort";
   } else {
+    // 不快指数の計算
+    float discomfortIndex = ((0.81 * latestSensorValue.temperature) + ((0.01 * latestSensorValue.humidity) * ((0.99 * latestSensorValue.temperature) - 14.3)) + 46.3);
     // 不快指数の画面表示
     if (discomfortIndex < 55 && discomfortStatus != "cold") {
       discomfortStatus = "cold";
@@ -259,7 +257,7 @@ void updateScreen(struct SensorValue latestSensorValue) {
   // eCO2とTVOCの値をシリアルモニタに通信する
   Serial.print("eCO2 "); Serial.print(sgp.eCO2); Serial.print("\t");
   Serial.print("TVOC "); Serial.print(sgp.TVOC); Serial.print("\n");
-
+  // 寝室モードのとき輝度を落とす
   if (bedroomModeFlg) {
     M5.Lcd.setBrightness(5);
   } else {
@@ -301,22 +299,22 @@ void setSpriteBackColor(struct StatusColor statusColor) {
 void updateLedBar() {
   struct ReminderThreshold eco2Threshold = {1000, 1500};
   // LEDの点灯処理
+  // 寝室モードのときのLED消灯処理
   if (bedroomModeFlg) {
-    // フラグが立っているときのLED消灯処理
     setLedColor(0, 0, 0, 0);
   } else {
     if (sgp.eCO2 > eco2Threshold.caution) {
-      // 警告
+      // 警告時のLED点灯と音声鳴動
       setLedColor(Eco2Color[1].color.r, Eco2Color[1].color.g, Eco2Color[1].color.b, 50);
       if (!cautionFlg) {
         playMp3("/1500ppm.mp3");
         cautionFlg = true;
       }
     } else if (sgp.eCO2 > eco2Threshold.attention) {
-      // 注意
+      // 注意時のLED点灯と音声鳴動
       setLedColor(Eco2Color[0].color.r, Eco2Color[0].color.g, Eco2Color[0].color.b, 25);
       if (!attentionFlg) {
-        playMp3("/1000ppm.mp3");
+        playMp3("/1000ppm.mp3");     
         attentionFlg = true;
         cautionFlg = false;
       }
@@ -378,9 +376,13 @@ void saveConfig() {
 
 // 計測した値をSDカードに保存する関数
 void saveLog(struct SensorValue latestSensorValue) {
+  // 時間をtm型で取得する変数
+  struct tm currentDateTime;
+  // ローカル時間を取得する
   bool getTime = getLocalTime(&currentDateTime);
   String measureDay = getDateString(currentDateTime);
   String measureTime = getTimeString(currentDateTime);
+  // SDカードにログを追加する
   File measureValues = SD.open("/measure_values.csv", FILE_APPEND);
   measureValues.print(measureDay + "," + measureTime + "," + sgp.eCO2 + "," + sgp.TVOC + "," + latestSensorValue.temperature + "," + latestSensorValue.humidity + "," + latestSensorValue.pressure + "\n");
   measureValues.close();
@@ -402,7 +404,6 @@ String getTimeString(struct tm currentDateTime) {
 void loop() {
   // 画面輝度調整の必須処理
   M5.update();
-
   // 計測
   struct SensorValue latestSensorValue = {0.0, 0.0, 0.0};
   measureSensorValues(latestSensorValue);
